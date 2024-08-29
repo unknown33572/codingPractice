c9a440819472951b785d8abf5d3a242f43f01a85
dishin <dishin@maitzsoft.co.kr>
2024-08-29 18:00:58 +0900

    정렬

--- src/main/java/com/example/Machine.java
+++ src/main/java/com/example/Machine.java
@@ -40,6 +40,8 @@ public class Machine {
 
 
     int[] numbers = {4, 5, 1, 3, 7, 4, 1};
+
+    mergeSort(numbers, 0, numbers.length - 1);
   }
 
   public static void bubbleSort(int[] arr) {
@@ -95,5 +97,89 @@ public class Machine {
       arr[minIndex] = arr[i];
       arr[i] = temp;
     }
+  }
+
+  public static void mergeSort(int[] arr, int l, int r) {
+    if (l < r) {
+      int m = l + (r - l) / 2;
+
+      mergeSort(arr, l, m);
+      mergeSort(arr, m + 1, r);
+
+      merge(arr, l, m, r);
+    }
+  }
+
+  public static void merge(int[] arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int[] L = new int[n1];
+    int[] R = new int[n2];
+
+    for (int i = 0; i < n1; i++) {
+      L[i] = arr[l + i];
+    }
+
+    for (int j = 0; j < n2; j++) {
+      R[j] = arr[m + 1 + j];
+    }
+
+    int i = 0, j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2) {
+      if (L[i] <= R[j]) {
+        arr[k] = L[i];
+        i++;
+      } else {
+        arr[k] = R[j];
+        j++;
+      }
+
+      k++;
+    }
+
+    while (i < n1) {
+      arr[k] = L[i];
+      i++;
+      k++;
+    }
+
+    while (j < n2) {
+      arr[k] = R[j];
+      j++;
+      k++;
+    }
+  }
+
+  public static void quickSort(int[] arr, int low, int high) {
+    if (low < high) {
+      int pi = partition(arr, low, high);
+
+      quickSort(arr, low, pi - 1);
+      quickSort(arr, pi + 1, high);
+    }
+  }
+
+  public static int partition(int[] arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+      if (arr[j] < pivot) {
+        i++;
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
   }
 }
