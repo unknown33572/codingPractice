ab3ca1ef02b36a37028b8c6a75acd5785f55a08c
dishin <dishin@maitzsoft.co.kr>
2024-09-04 20:17:13 +0900

    프로그래머스

--- src/main/java/com/example/practice.js
+++ src/main/java/com/example/practice.js
@@ -91,6 +91,7 @@ const arr2 = [[3,3],[3,3]];
 console.log(solution(arr1, arr2));
 */
 
+/*
 function solution(N, stages) {
   const challenger = new Array(N + 2).fill(0);
   // for (const stage of stages) {
@@ -116,3 +117,185 @@ function solution(N, stages) {
     return result.map((v) => Number(v[0]));
   }
 }
+*/
+
+/*
+function solution(dirs) {
+  const visited = new Set();
+  const move = {
+    U: [0, 1],
+    D: [0, -1],
+    R: [1, 0],
+    L: [-1, 0],
+  }
+
+  let x = 0;
+  let y = 0;
+
+  for (const dir of dirs) {
+    const [dx, dy] = move[dir];
+    const nx = x + dx;
+    const ny = y + dy;
+
+    if (!(nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5)) {
+      continue;
+    }
+
+    visited.add(`${x}${y}${nx}${ny}`);
+    visited.add(`${nx}${ny}${x}${y}`);
+
+    x = nx;
+    y = ny;
+
+  }
+
+  return visited.size / 2;
+}
+*/
+
+/*
+const stack = [];
+const maxSize = 5;
+
+function isFull() {
+  return stack.length === maxSize;
+}
+
+function isEmpty() {
+  return stack.length === 0;
+}
+
+function push(stack, value) {
+  if (isFull(stack)) {
+    console.log('스택이 꽉 찼습니다.');
+    return;
+  } else {
+    console.log('스택에 데이터를 추가합니다.');
+    stack.push(value);
+  }
+}
+
+function pop(stack) {
+  if (isEmpty(stack)) {
+    console.log('스택이 비었습니다.');
+    return null;
+  } else {
+    console.log('스택에서 데이터를 추출합니다.');
+    return stack.pop();
+  }
+}
+
+let diverseSet = new Set();
+diverseSet.add('text');
+diverseSet.add(123);
+diverseSet.add({key: 'value'});
+diverseSet.add([1, 2, 3]);
+
+console.log(diverseSet);
+*/
+
+/*
+function parenthesesChecker(value) {
+  const stack = [];
+  const open = '({[<';
+  const close = ')}]>';
+
+  for (const char of value) {
+    if (open.includes(char)) {
+      stack.push(char);
+    } else if (close.includes(char)) {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    } 
+  }
+
+  return stack.length === 0;
+}
+
+console.log(parenthesesChecker('<{[(<[]}>]>)'));
+
+*/
+
+/*
+function solution(decimal) {
+  const stack = [];
+
+  while (decimal > 0) {
+    stack.push(decimal % 2);
+    decimal = Math.floor(decimal / 2);
+  }
+
+  let binary = '';
+  while (stack.length > 0) {
+    binary += stack.pop();
+  }
+
+  return binary;
+}
+
+console.log(solution(10));
+*/
+
+/*
+function solution(s) {
+  const n = s.length;
+  let answer = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const stack = [];
+    let isCorrect = true;
+    for (let j = 0; j < n; j++) {
+      const current =  s[(i + j) % n];
+
+      if (current === '(' || current === '[' || current === '{') {
+        stack.push(current);
+      } else {
+        if (stack.length === 0) {
+          isCorrect = false;
+          break;
+        }
+
+        const top = stack[stack.length - 1];
+
+        if ((current === ')' && top === '(') ||
+            (current === ']' && top === '[') ||
+            (current === '}' && top === '{')) {
+          stack.pop();
+        } else {
+          isCorrect = false;
+          break;
+        }
+      }
+    }
+    if (isCorrect && stack.length === 0) {
+      answer++;
+    }
+  }
+  return answer;
+}
+
+const s = '[](){}';
+
+console.log(solution(s));
+*/
+
+function solution(string) {
+  const stack = [];
+
+  for (const char of string) {
+    if (stack.length > 0 && stack[stack.length - 1] === char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
+
+const s = 'baabaab';
+
+console.log(solution(s));
